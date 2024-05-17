d534d1618e543243c1b3d6685c085f6c1e4dc62e
Luca Tremamunno <tremaluca96@gmail.com>
2024-05-17 14:38:09 +0200

    Use caffeine cache

--- lite-core/src/main/kotlin/org/taktik/icure/config/LiteCoreConfig.kt
+++ lite-core/src/main/kotlin/org/taktik/icure/config/LiteCoreConfig.kt
@@ -2,10 +2,13 @@ package org.taktik.icure.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.taktik.icure.properties.IcureEntitiesCacheProperties
 import org.taktik.icure.spring.asynccache.AsyncMapCacheManager
 
 @Configuration
 class LiteCoreConfig {
     @Bean
-    fun asyncCacheManager() = AsyncMapCacheManager()
+    fun asyncCacheManager(
+        entitiesCacheProperties: IcureEntitiesCacheProperties
+    ) = AsyncMapCacheManager(entitiesCacheProperties)
 }
