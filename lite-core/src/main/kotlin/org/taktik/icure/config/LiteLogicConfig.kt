f131e1bdafc57bb9221979ed749b4153f3253d84
aduchate <ad@icure.com>
2024-06-27 11:48:41 +0200

    Fix beans annotations

--- lite-core/src/main/kotlin/org/taktik/icure/config/LiteLogicConfig.kt
+++ lite-core/src/main/kotlin/org/taktik/icure/config/LiteLogicConfig.kt
@@ -4,21 +4,44 @@
 
 package org.taktik.icure.config
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.taktik.couchdb.id.UUIDGenerator
 import org.taktik.icure.asyncdao.CodeDAO
+import org.taktik.icure.asyncdao.ContactDAO
+import org.taktik.icure.asyncdao.DocumentDAO
+import org.taktik.icure.asyncdao.FormDAO
+import org.taktik.icure.asyncdao.HealthElementDAO
+import org.taktik.icure.asyncdao.HealthcarePartyDAO
 import org.taktik.icure.asyncdao.InsuranceDAO
+import org.taktik.icure.asyncdao.InvoiceDAO
+import org.taktik.icure.asyncdao.MessageDAO
+import org.taktik.icure.asyncdao.PatientDAO
 import org.taktik.icure.asyncdao.TarificationDAO
 import org.taktik.icure.asyncdao.UserDAO
+import org.taktik.icure.asynclogic.EntityReferenceLogic
+import org.taktik.icure.asynclogic.ExchangeDataMapLogic
+import org.taktik.icure.asynclogic.InsuranceLogic
 import org.taktik.icure.asynclogic.SessionInformationProvider
 import org.taktik.icure.asynclogic.UserLogic
 import org.taktik.icure.asynclogic.datastore.DatastoreInstanceProvider
 import org.taktik.icure.asynclogic.impl.CodeLogicImpl
+import org.taktik.icure.asynclogic.impl.ContactLogicImpl
+import org.taktik.icure.asynclogic.impl.DocumentLogicImpl
+import org.taktik.icure.asynclogic.impl.FormLogicImpl
+import org.taktik.icure.asynclogic.impl.HealthElementLogicImpl
 import org.taktik.icure.asynclogic.impl.InsuranceLogicImpl
+import org.taktik.icure.asynclogic.impl.InvoiceLogicImpl
+import org.taktik.icure.asynclogic.impl.MessageLogicImpl
+import org.taktik.icure.asynclogic.impl.PatientLogicImpl
 import org.taktik.icure.asynclogic.impl.SessionInformationProviderImpl
 import org.taktik.icure.asynclogic.impl.TarificationLogicImpl
 import org.taktik.icure.asynclogic.impl.UserLogicImpl
 import org.taktik.icure.asynclogic.impl.filter.Filters
+import org.taktik.icure.asynclogic.objectstorage.DocumentDataAttachmentLoader
+import org.taktik.icure.asynclogic.objectstorage.DocumentDataAttachmentModificationLogic
 import org.taktik.icure.properties.CouchDbPropertiesImpl
 import org.taktik.icure.security.SessionAccessControlKeysProvider
 import org.taktik.icure.security.credentials.SecretValidator
@@ -26,9 +49,7 @@ import org.taktik.icure.security.user.UserEnhancer
 import org.taktik.icure.validation.aspect.Fixer
 
 @Configuration
-class LiteLogicConfig(
-
-){
+class LiteLogicConfig {
     @Bean
     fun sessionInformationProvider(
         sessionAccessControlKeysProvider: SessionAccessControlKeysProvider,
@@ -39,7 +60,6 @@ class LiteLogicConfig(
     @Bean
     fun userLogic(
         datastoreInstanceProvider: DatastoreInstanceProvider,
-        couchDbProperties: CouchDbPropertiesImpl,
         userDAO: UserDAO,
         secretValidator: SecretValidator,
         filters: Filters,
@@ -89,4 +109,81 @@ class LiteLogicConfig(
         datastoreInstanceProvider,
         fixer
     )
+
+    @Bean
+    fun contactLogic(
+        contactDAO: ContactDAO,
+        exchangeDataMapLogic: ExchangeDataMapLogic,
+        sessionLogic: SessionInformationProvider,
+        datastoreInstanceProvider: DatastoreInstanceProvider,
+        filters: Filters,
+        fixer: Fixer
+    ) = ContactLogicImpl(contactDAO, exchangeDataMapLogic, sessionLogic, datastoreInstanceProvider, filters, fixer)
+
+    @Bean
+    fun documentLogic(
+        documentDAO: DocumentDAO,
+        sessionLogic: SessionInformationProvider,
+        datastoreInstanceProvider: DatastoreInstanceProvider,
+        exchangeDataMapLogic: ExchangeDataMapLogic,
+        attachmentModificationLogic: DocumentDataAttachmentModificationLogic,
+        @Qualifier("documentDataAttachmentLoader") attachmentLoader: DocumentDataAttachmentLoader,
+        fixer: Fixer
+    ) = DocumentLogicImpl(documentDAO, sessionLogic, datastoreInstanceProvider, exchangeDataMapLogic, attachmentModificationLogic, attachmentLoader, fixer)
+
+    @Bean
+    fun formLogic(
+        formDAO: FormDAO,
+        exchangeDataMapLogic: ExchangeDataMapLogic,
+        sessionLogic: SessionInformationProvider,
+        datastoreInstanceProvider: DatastoreInstanceProvider,
+        fixer: Fixer
+    ) = FormLogicImpl(formDAO, exchangeDataMapLogic, sessionLogic, datastoreInstanceProvider, fixer)
+
+    @Bean
+    fun healthElementLogic(
+        healthElementDAO: HealthElementDAO,
+        filters: Filters,
+        sessionLogic: SessionInformationProvider,
+        exchangeDataMapLogic: ExchangeDataMapLogic,
+        datastoreInstanceProvider: DatastoreInstanceProvider,
+        fixer: Fixer
+    ) = HealthElementLogicImpl(filters, healthElementDAO, sessionLogic, exchangeDataMapLogic, datastoreInstanceProvider, fixer)
+
+    @Bean
+    fun invoiceLogic(
+        filters: Filters,
+        userLogic: UserLogic,
+        insuranceLogic: InsuranceLogic,
+        uuidGenerator: UUIDGenerator,
+        entityReferenceLogic: EntityReferenceLogic,
+        invoiceDAO: InvoiceDAO,
+        sessionLogic: SessionInformationProvider,
+        exchangeDataMapLogic: ExchangeDataMapLogic,
+        datastoreInstanceProvider: DatastoreInstanceProvider,
+        fixer: Fixer
+    ) = InvoiceLogicImpl(filters, userLogic, insuranceLogic, uuidGenerator, entityReferenceLogic, invoiceDAO, sessionLogic, exchangeDataMapLogic, datastoreInstanceProvider, fixer)
+
+    @Bean
+    fun messageLogic(
+        messageDAO: MessageDAO,
+        exchangeDataMapLogic: ExchangeDataMapLogic,
+        sessionLogic: SessionInformationProvider,
+        datastoreInstanceProvider: DatastoreInstanceProvider,
+        filters: Filters,
+        userLogic: UserLogic,
+        fixer: Fixer
+    ) = MessageLogicImpl(messageDAO, exchangeDataMapLogic, sessionLogic, datastoreInstanceProvider, filters, userLogic, fixer)
+
+    @Bean
+    fun patientLogic(
+        healthcarePartyDAO: HealthcarePartyDAO,
+        sessionLogic: SessionInformationProvider,
+        patientDAO: PatientDAO,
+        userLogic: UserLogic,
+        filters: Filters,
+        exchangeDataMapLogic: ExchangeDataMapLogic,
+        datastoreInstanceProvider: org.taktik.icure.asynclogic.datastore.DatastoreInstanceProvider,
+        fixer: Fixer
+    ) = PatientLogicImpl(sessionLogic, patientDAO, userLogic, filters, exchangeDataMapLogic, datastoreInstanceProvider, fixer)
 }
