617713a14c75d56915f2b0c4442c81808ee72d27
Vincenzo Pierro <vcp@icure.com>
2024-06-28 18:04:43 +0200

    Added encoder as external plugin

---
+++ lite-core/src/main/kotlin/org/taktik/icure/config/PluginsManager.kt
@@ -0,0 +1,54 @@
+package org.taktik.icure.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.net.URI
+import java.net.URLClassLoader
+import java.io.File
+import java.util.jar.JarFile
+
+@Component
+class PluginsManager(
+	@Value("\${icure.lite.plugins.sourceFolder:#{null}}") private val sourceFolder: String? = null
+) {
+	private val log = LoggerFactory.getLogger(this.javaClass)
+
+	class InvalidPluginException(className: String) : IllegalArgumentException("Invalid plugin $className")
+
+	init {
+		println(sourceFolder)
+	}
+
+	private val loadedClasses = sourceFolder?.let { folderPath ->
+		log.info("Loading kraken plugins")
+		val pluginsFolder = File(folderPath)
+		if (pluginsFolder.exists()) {
+			val jarFiles = pluginsFolder
+				.walk()
+				.filter { it.isFile && it.name.endsWith(".jar") }
+				.associate { pluginFile ->
+					log.info("Loading ${pluginFile.name} plugin")
+					URI.create("jar:file:${pluginFile.absolutePath}!/") to JarFile(pluginFile)
+				}
+
+			val parentClassLoader = this::class.java.classLoader
+			val classLoader = URLClassLoader.newInstance(jarFiles.keys.map { it.toURL() }.toTypedArray(), parentClassLoader)
+			jarFiles.values.flatMap { jarFile ->
+				jarFile.entries().asSequence().filter {
+					it.name.matches("^(.+/)+[^$]+\\.class$".toRegex())
+				}.map { entry ->
+					entry.name.replace(".class", "").replace("/", ".")
+				}.toList()
+			}.associateWith { className -> classLoader.loadClass(className) }
+		} else null
+	} ?: emptyMap()
+
+
+	fun instantiate(className: String, vararg args: Any?) =
+		loadedClasses[className]?.getDeclaredConstructor(*args.map { it?.javaClass ?: Nothing::class.java }.toTypedArray())?.newInstance(*args)
+			?: throw InvalidPluginException(className)
+}
+
+inline fun <reified T : Any> PluginsManager.newInstance(className: String, vararg args: Any?): T =
+	instantiate(className, *args) as T
