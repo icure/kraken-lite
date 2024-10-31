1e99151c9db6b15f07eba624c7c4bd5a0418bea2
aduchate <ad@icure.com>
2024-10-31 20:34:49 +0100

    Make it compile

--- lite-core/src/main/kotlin/org/taktik/icure/security/user/NoEnhancerImpl.kt
+++ lite-core/src/main/kotlin/org/taktik/icure/security/user/NoEnhancerImpl.kt
@@ -6,17 +6,18 @@ import kotlinx.coroutines.flow.transform
 import org.springframework.stereotype.Component
 import org.taktik.couchdb.ViewQueryResultEvent
 import org.taktik.couchdb.ViewRowWithDoc
+import org.taktik.icure.entities.EnhancedUser
 import org.taktik.icure.entities.User
 
 @Component
 class NoUserEnhancer : UserEnhancer {
-    override suspend fun enhance(user: User) = user.enhanceWith(null)
+    override suspend fun enhance(user: User, includeMetadataFromGlobalUser: Boolean) = user.enhanceWith(null)
 
-    override suspend fun enhanceFlow(usersFlow: Flow<User>) = usersFlow.map {
+    override suspend fun enhanceFlow(usersFlow: Flow<User>, includeMetadataFromGlobalUser: Boolean) = usersFlow.map {
         it.enhanceWith(null)
     }
 
-    override suspend fun enhanceViewFlow(usersFlow: Flow<ViewQueryResultEvent>) = usersFlow.transform {
+    override suspend fun enhanceViewFlow(usersFlow: Flow<ViewQueryResultEvent>, includeMetadataFromGlobalUser: Boolean) = usersFlow.transform {
         if (it is ViewRowWithDoc<*, *, *>) {
             @Suppress("UNCHECKED_CAST")
             emit((it as ViewRowWithDoc<*, *, User>).copy(doc = it.doc.enhanceWith(null)))
