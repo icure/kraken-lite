008ceea5dc4218553dd19fb8cec9e50f1fe40ea8
Vincenzo Pierro <vcp@icure.com>
2025-01-23 15:13:58 +0100

    Updated lite service

--- lite-core/src/main/kotlin/org/taktik/icure/asyncservice/impl/DataOwnerServiceImpl.kt
+++ lite-core/src/main/kotlin/org/taktik/icure/asyncservice/impl/DataOwnerServiceImpl.kt
@@ -1,5 +1,6 @@
 package org.taktik.icure.asyncservice.impl
 
+import kotlinx.coroutines.flow.Flow
 import org.springframework.stereotype.Service
 import org.taktik.icure.asynclogic.DataOwnerLogic
 import org.taktik.icure.asyncservice.DataOwnerService
@@ -11,8 +12,10 @@ class DataOwnerServiceImpl(
     private val dataOwnerLogic: DataOwnerLogic
 ) : DataOwnerService {
     override suspend fun getCryptoActorStub(dataOwnerId: String): CryptoActorStubWithType? = dataOwnerLogic.getCryptoActorStub(dataOwnerId)
-
+    override fun getCryptoActorStubs(dataOwnerIds: List<String>): Flow<CryptoActorStubWithType> = dataOwnerLogic.getCryptoActorStubs(dataOwnerIds)
     override suspend fun getDataOwner(dataOwnerId: String): DataOwnerWithType? = dataOwnerLogic.getDataOwner(dataOwnerId)
-
+    override fun getDataOwners(dataOwnerIds: List<String>): Flow<DataOwnerWithType> = dataOwnerLogic.getDataOwners(dataOwnerIds)
     override suspend fun modifyCryptoActor(modifiedCryptoActor: CryptoActorStubWithType): CryptoActorStubWithType = dataOwnerLogic.modifyCryptoActor(modifiedCryptoActor)
+    override fun getCryptoActorHierarchy(dataOwnerId: String): Flow<DataOwnerWithType> = dataOwnerLogic.getCryptoActorHierarchy(dataOwnerId)
+    override fun getCryptoActorHierarchyStub(dataOwnerId: String): Flow<CryptoActorStubWithType> = dataOwnerLogic.getCryptoActorHierarchyStub(dataOwnerId)
 }
