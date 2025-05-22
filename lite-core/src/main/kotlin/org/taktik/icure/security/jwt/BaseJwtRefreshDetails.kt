ac26dae7cd4380233b9caa2769f91fbbd247bf7e
Vincenzo Pierro <vcp@icure.com>
2025-05-22 14:09:55 +0200

    Fixes after common update

--- lite-core/src/main/kotlin/org/taktik/icure/security/jwt/BaseJwtRefreshDetails.kt
+++ lite-core/src/main/kotlin/org/taktik/icure/security/jwt/BaseJwtRefreshDetails.kt
@@ -1,26 +1,30 @@
 package org.taktik.icure.security.jwt
 
-import io.jsonwebtoken.Claims
+import org.springframework.stereotype.Component
 import org.taktik.icure.exceptions.InvalidJwtException
+import org.taktik.icure.properties.AuthProperties
+import java.time.Instant
+
+@Component
+class BaseRefreshJwtConverter(
+    private val authProperties: AuthProperties
+) : JwtConverter<JwtRefreshDetails> {
+
+    override fun fromClaims(claims: Map<String, Any?>): JwtRefreshDetails =
+        BaseJwtRefreshDetails(
+            userId = (claims[USER_ID] as String?) ?: throw InvalidJwtException("Missing user id from claims"),
+            jwtDuration = (claims[JWT_DURATION] as? Int?)?.toLong() ?: authProperties.jwt.refreshExpirationSeconds,
+            expiration = (claims[Jwt.StandardClaims.EXPIRES_AT] as Instant).epochSecond
+        )
+
+}
 
 data class BaseJwtRefreshDetails(
     override val userId: String,
-    override val jwtDuration: Long
+    override val jwtDuration: Long,
+    override val expiration: Long? = null
 ) : JwtRefreshDetails {
 
-    companion object : JwtConverter<JwtRefreshDetails> {
-
-        override fun fromClaims(claims: Claims, jwtExpirationTime: Long): JwtRefreshDetails =
-            BaseJwtRefreshDetails(
-                userId = (claims[USER_ID] as String?) ?: throw InvalidJwtException("Missing user id from claims"),
-                jwtDuration = (claims[JWT_DURATION] as? Int?)?.toLong() ?: jwtExpirationTime
-            )
-
-    }
-
-    override fun toClaims(): Map<String, Any?> = mapOf(
-        USER_ID to userId,
-        JWT_DURATION to jwtDuration,
-    )
+    override fun toClaimsOmittingExpiration(): Map<String, Any?> = mapOf(USER_ID to userId, JWT_DURATION to jwtDuration)
 
 }
